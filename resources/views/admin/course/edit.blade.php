0f952d646eb5b069f0532c6cb4a8e52350124a5d
dimas551 <ahmaddimas5151@gmail.com>
2024-12-17 11:54:25 +0700

    ClassXpert

--- resources/views/admin/course/edit.blade.php
+++ resources/views/admin/course/edit.blade.php
@@ -157,10 +157,10 @@
 
                                     <div class="container-fluid mt-2">
                                         <div class="row">
-                                            <div class="col-md-6">
+                                            <div class="col-md-12 mt-4 mb-4">
                                                 <div class="d-flex justify-content-between align-items-center">
                                                     <h4 class="text-primary mb-0">Material</h4>
-                                                    <a class="rbt-btn btn-sm" data-bs-toggle="modal"
+                                                    <a class="rbt-btn btn-sm ms-auto" data-bs-toggle="modal"
                                                         data-bs-target="#materialModal">
                                                         <span>Create Material</span>
                                                     </a>
