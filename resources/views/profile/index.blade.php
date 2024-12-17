679198320a11795c41e64fed3ed480039df3fd1d
dimas551 <ahmaddimas5151@gmail.com>
2024-12-17 11:25:52 +0700

    ClassXpert

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -19,12 +19,20 @@
                     <div class="profile-content rbt-shadow-box">
                         <div class="rbt-title-style-3 d-flex justify-content-between align-items-center">
                             <h4 class="mb-0">About Me</h4>
-                            @if (request()->route('username') === Auth::user()->username)
+                            @if (Auth::check() && Auth::user()->username === $user->username)
                                 <a class="rbt-btn btn-border btn-sm radius-round-6"
-                                    href="{{ route('profile.edit', Auth::user()->id) }}">
+                                    href="{{ route('profile.edit', $user->id) }}">
                                     <i class="feather-edit"></i> Edit
                                 </a>
                             @endif
+                        </div>
+
+                        <div class="row g-5">
+                            <div class="col-lg-12">
+                                <p class="mt--10 mb--20">
+                                    {{ $user->bio ?? '' }}
+                                </p>
+                            </div>
                         </div>
                     </div>
 
